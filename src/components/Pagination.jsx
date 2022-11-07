9833deb8cd4039d8e451967a134729a82b89e553
codeInn001 <emmaalege@gmail.com>
2022-11-08 00:03:53 +0100

    added pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import '../assets/styles/pagination.css';
+
+
+
+
+function Pagination({ pageNumbers, currentPage, setCurrentPage }) {
+  const numbersOfPages = [...Array(pageNumbers).keys()].slice(1);
+
+  function nextPage() {
+    if (currentPage !== numbersOfPages.length)
+      setCurrentPage((prev) => prev + 1);
+  }
+
+  function prevPage() {
+    if (currentPage !== 1) setCurrentPage((prev) => prev - 1);
+  }
+  return (
+    <div className="pagination-container">
+      <button className="previous-btn" onClick={prevPage}>
+        Previous
+      </button>
+      {numbersOfPages.map((page) => (
+        <button
+          className="numbered-btn"
+          key={page}
+          onClick={() => setCurrentPage(page)}
+        >
+          {page}
+        </button>
+      ))}
+      <button className="next-btn" onClick={nextPage}>
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -3,13 +3,18 @@ import { useEffect, useState } from "react";
 import "../assets/styles/repositories.css";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
-
-
+import Pagination from "./Pagination";
 
 function Repositories({ repos }) {
-  // const [repos, setRepos] = useState(null)
-  // const [loading, setLoading] = useState(false)
-  // const [error, setError] = useState(null)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [reposPerPage] = useState(6);
+
+  const indexOfLastRepo = currentPage * reposPerPage;
+  const IndexOfFirstRepo = indexOfLastRepo - reposPerPage;
+
+  const currentRepos = repos.slice(IndexOfFirstRepo, indexOfLastRepo);
+  const pageNumbers = Math.ceil(repos.length / reposPerPage);
+
   const showLess = (description) => {
     if (description.length > 100) {
       return description.slice(0, 100) + "...";
@@ -18,35 +23,43 @@ function Repositories({ repos }) {
   };
 
   return (
-    <section className="repo-section">
-      {repos === "loading" ? (
-        <h1>Loading...</h1>
-      ) : (
-        repos.map((repo) => {
-          return (
-            <div className="repo" key={repo.id}>
-              <Helmet>
-                <meta charSet="utf-8" />
-                <title>Codeinn001 Github Repositiries</title>
-                <link rel="canonical" href="/" />
-                <meta
-                  name="description"
-                  content="View codeinn001 github repositories"
-                />
-              </Helmet>
-              <h2>{repo.name}</h2>
-              <p>{repo.description ? showLess(repo.description) : ""}</p>
-              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                View Repo
-              </a>
-              <Link className="nav-link" to="/repoInfo">
-                Repository details
-              </Link>
-            </div>
-          );
-        })
-      )}
-    </section>
+    <div>
+        <section className="repo-section">
+          {repos === "loading" ? (
+            <h1>Loading...</h1>
+          ) : (
+            currentRepos.map((repo) => {
+              return (
+                <div className="repo" key={repo.id}>
+                  <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Codeinn001 Github Repositiries</title>
+                    <link rel="canonical" href="/" />
+                    <meta
+                      name="description"
+                      content="View codeinn001 github repositories"
+                    />
+                  </Helmet>
+                  <h2>{repo.name}</h2>
+                  <p>{repo.description ? showLess(repo.description) : ""}</p>
+                  <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                    View Repo
+                  </a>
+                  <Link className="nav-link" to="/repoInfo">
+                    Repository details
+                  </Link>
+                </div>
+              );
+            })
+          )}
+        
+        </section>
+        <Pagination
+        pageNumbers={pageNumbers}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
   );
 }
 
