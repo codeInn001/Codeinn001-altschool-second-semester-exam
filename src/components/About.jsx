f2235c35fe723284540efd0ceecba7b28dc3e69f
codeInn001 <emmaalege@gmail.com>
2022-11-07 11:41:06 +0100

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import '../assets/styles/about.css'
+import '../assets/styles/errorboundary.css'
 
 function About() {
   return (
     <div className='about-container'>
-        error 404
+       <h1>page does not exist </h1>
+          <h3>404 ☹️</h3>
+          <a href='/'>Go back home</a>
     </div>
   )
 }
